2e738888fbbfa61e0e3a106b352f15e8857096cf
Laurent Raufaste <analogue@glop.org>
2015-12-04 13:12:11 -0500

    PEP8 please

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from google.appengine.ext import ndb
 
+
 class Coffee(ndb.Model):
     name = ndb.StringProperty(required=True)
     desc = ndb.StringProperty(required=True)
@@ -8,18 +9,23 @@ class Coffee(ndb.Model):
     last_updated_at = ndb.DateTimeProperty(auto_now=True, required=True)
 
     def to_json_full_dict(self):
-        return {"id": self.key.id(),
-                "name": self.name,
-                "desc": self.desc,
-                "image_url": self.image_url,
-                "last_updated_at": str(self.last_updated_at),
-               }
+        return {
+            "id": self.key.id(),
+            "name": self.name,
+            "desc": self.desc,
+            "image_url": self.image_url,
+            "last_updated_at": str(self.last_updated_at),
+        }
 
     def to_json_summary_dict(self):
-        return {"id": self.key.id(),
-                "name": self.name,
-                "desc": self.desc[:100] + "..." if len(self.desc) > 100 else self.desc,
-                "image_url": self.image_url,
-               }
+        if len(self.desc) > 100:
+            desc = self.desc[:100] + "..."
+        else:
+            desc = self.desc
 
-
+        return {
+            "id": self.key.id(),
+            "name": self.name,
+            "desc": desc,
+            "image_url": self.image_url,
+        }
